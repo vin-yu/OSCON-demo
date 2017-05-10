7e1cb4de6c177ebbea5c6c8a2860a0e9d8a69974
root <root@OSCon.0jw404d4hpredf1bgeargf4urg.jx.internal.cloudapp.net>
2017-05-10 00:52:28 +0000

    edits

--- aquarium-v2/app/setup/setup.sql
+++ aquarium-v2/app/setup/setup.sql
@@ -1,27 +1,7 @@
-﻿IF EXISTS(select * from sys.databases where name='CommentsDb')
-DROP DATABASE CommentsDb
-GO
-
-CREATE DATABASE CommentsDb;
-GO
-
-USE CommentsDb;
-GO
-
-DROP TABLE IF EXISTS Comments
-GO
-
-CREATE TABLE Comments (
-	id int IDENTITY PRIMARY KEY,
-	author nvarchar(30) NOT NULL,
-	text nvarchar(4000),
-	species varchar(32) --political correctness
-)
+﻿Alter TABLE Comments ADD Column species varchar(32) --political correctness
 GO
 
 INSERT INTO Comments (author, text, species)
 VALUES
-('Luis','Hello DockerCon!', 'whale'),
-('Luis','I''m a whale.', 'whale'),
 ('Someone else','and I''m something else.', 'octopus')
 GO
